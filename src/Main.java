ba77302f9b824261fdf7cf57418ed3e8436edf27
Hw2 <Kucherenko.vv@mail.ru>
2023-03-04 23:37:06 +0800

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //#1
+        Stream<Integer> stream = Stream.of(3, 7, 4, 9, 1, 6);
+
+        findMinMax(
+                stream,
+                Comparator.naturalOrder(),
+                (x, y) -> System.out.printf("Min: %s, Max: %s%n", x, y)
+        );
+
+        //#2
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,10,12,14,15));
+
+        System.out.println("Count of even element: " + evenNumber(list));
+
+        stream.close();
+    }
+
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> items = stream.sorted(order).collect(Collectors.toList());
+        if (!items.isEmpty()) {
+            minMaxConsumer.accept(items.get(0), items.get(items.size() - 1));
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+    }
+
+    public static int evenNumber (List <Integer> numbers) {
+        return (int) numbers.stream()
+                .filter(s -> s % 2 ==0)
+                .collect(Collectors.toList())
+                .stream().count();
     }
 }
